51d6f572f810ded928d552738a17d20743c8e8d1
h.f.chau1126 <118866662+hfchau1126@users.noreply.github.com>
2025-12-30 13:01:01 +0800

    Update multiprocessing_module.py

--- src/multiprocessing_module.py
+++ src/multiprocessing_module.py
@@ -2,14 +2,14 @@ import time
 import multiprocessing
 from . import utils
 
-def run_multiprocessing(image_paths, output_dir, num_processes=None):
+def run_multiprocessing(image_paths, output_dir, num_processes=None, prefix="mp"):
     if num_processes is None:
         num_processes = multiprocessing.cpu_count()
         
     print(f"Starting Multiprocessing with {num_processes} processes for {len(image_paths)} images...")
     start_time = time.time()
     
-    tasks = [(path, output_dir) for path in image_paths]
+    tasks = [(path, output_dir, f"{prefix}_{num_processes}") for path in image_paths]
     
     with multiprocessing.Pool(processes=num_processes) as pool:
         pool.map(utils.process_single_image, tasks)
