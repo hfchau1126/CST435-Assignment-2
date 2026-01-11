050dc01b56d533b0e17dec9c36d8bb75ac424449
h.f.chau1126 <118866662+hfchau1126@users.noreply.github.com>
2026-01-11 22:26:59 +0800

    Update concurrent_futures_module.py

--- src/concurrent_futures_module.py
+++ src/concurrent_futures_module.py
@@ -3,7 +3,9 @@ import os
 import concurrent.futures
 from . import utils
 
+# Run image processing using concurrent.futures.ThreadPoolExecutor
 def run_futures(image_paths, output_dir, max_workers=None, prefix="fut"):
+
     if max_workers is None:
         max_workers = os.cpu_count()
         
@@ -12,6 +14,7 @@ def run_futures(image_paths, output_dir, max_workers=None, prefix="fut"):
     
     tasks = [(path, output_dir, f"{prefix}_{max_workers}") for path in image_paths]
     
+    # Use ThreadPoolExecutor to manage a pool of threads
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         executor.map(utils.process_single_image, tasks)
         
